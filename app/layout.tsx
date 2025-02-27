b6e03e59b9200c6ce2e23dce4f8f1dfb71d2a765
nranabhat <78765060+nranabhat@users.noreply.github.com>
2025-02-26 20:24:10 -0600

    using new favicon url

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
   description: 'Identify plants using AI technology',
   icons: {
     icon: [
-      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
